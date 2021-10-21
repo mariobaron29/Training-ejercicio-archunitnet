116aa8d209e474c079d2a7ac00bea5046b0d7652
Mario Julian Barón Rueda <mario.baron@sofka.com.co>
2021-10-21 09:44:02 -0500

    Creación del proyecto

---
+++ demo_archunitnet/demo_archunitnet_tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace demo_archunitnet_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
